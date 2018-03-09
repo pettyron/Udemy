990ed10f638f8ce85fef46ed676643ff17f89467
Ronnie Petty <rpetty.developer@gmail.com>
2018-03-08 20:17:40 -0600

    add another filter example

--- es6_javascript/array_helpers/filter_explained/app/scripts.js
+++ es6_javascript/array_helpers/filter_explained/app/scripts.js
@@ -34,3 +34,20 @@ var newProductList = products2.filter(function (product) {
 });
 
 console.log(newProductList);
+
+// Another filter example that matches posts and comments
+
+var post = {id: 4, title: 'New Post'};
+var comments = [
+  {postId: 4, content: 'awesome post'},
+  {postId: 3, content: 'it was ok'},
+  {postId: 4, content: 'neat'}
+];
+
+function commentsForPost(post, comments) {
+  return comments.filter(function (comment) {
+    return comment.postId === post.id;
+  });
+}
+
+commentsForPost(post, comments);
